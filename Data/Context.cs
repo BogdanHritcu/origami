8edf341607dacf4de3044a8cf6b396a216351093
BogdanHritcu <hritcubogdan99@gmail.com>
2021-12-06 00:54:03 +0200

    added user model and the base db context

---
+++ Data/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using origami_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace origami_backend.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public Context(DbContextOptions<Context> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using origami_backend.Models.Base;
+
+namespace origami_backend.Models
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; }
+        public string Displayname { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Picture { get; set; }
+        public string Name { get; set; }
+        public DateTime? Birthday { get; set; }
+    }
+}
