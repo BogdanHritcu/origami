00f43447b4f2b6d2298296839ecbbb1033f710dd
BogdanHritcu <hritcubogdan99@gmail.com>
2021-12-09 18:44:39 +0200

    added profile model

---
+++ Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace origami_backend.Models
+{
+    public enum Theme { Dark, Light }
+    public class Profile : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public string Displayname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string PicturePath { get; set; }
+        public string Description { get; set; }
+        public Theme Theme { get; set; }
+    }
+}
