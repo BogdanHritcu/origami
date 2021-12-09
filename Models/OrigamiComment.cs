94e6c284e901596181361c2c3ece3f3dc6ec5de6
BogdanHritcu <hritcubogdan99@gmail.com>
2021-12-09 18:59:52 +0200

    added origami comment model

---
+++ Models/OrigamiComment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace origami_backend.Models
+{
+    public class OrigamiComment : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public Guid OrigamiId { get; set; }
+        public string Body { get; set; }
+    }
+}
