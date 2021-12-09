bf5501dbc7848063af5c82b738c99b95ebcbc298
BogdanHritcu <hritcubogdan99@gmail.com>
2021-12-09 18:50:25 +0200

    added study model

---
+++ Models/Study.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace origami_backend.Models
+{
+    public enum Status { Planned, Working, Done, GaveUp }
+    public class Study : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public Guid OrigamiId { get; set; }
+        public Status Status { get; set; }
+        public int Score { get; set; }
+        public bool Favorite { get; set; }
+        public DateTime? DateStarted { get; set; }
+        public DateTime? DateFinished { get; set; }
+    }
+}
