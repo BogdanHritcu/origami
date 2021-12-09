e14def7910787ddb72ad4a59c5aed99d2edb18eb
BogdanHritcu <hritcubogdan99@gmail.com>
2021-12-09 18:53:38 +0200

    added step model

---
+++ Models/Step.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace origami_backend.Models
+{
+    public class Step : BaseEntity
+    {
+        public Guid OrigamiId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PicturePath { get; set; }
+        public int Number { get; set; }
+    }
+}
