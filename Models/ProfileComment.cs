819a706361fd23ccdea5022fa1f3365f4c8d4fa2
BogdanHritcu <hritcubogdan99@gmail.com>
2021-12-09 19:00:58 +0200

    added profile comment model

---
+++ Models/ProfileComment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace origami_backend.Models
+{
+    public class ProfileComment : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public Guid ProfileId { get; set; }
+        public string Body { get; set; }
+    }
+}
