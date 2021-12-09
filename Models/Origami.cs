0621e4653b68932964fbc249b8c5bd1c93beea98
BogdanHritcu <hritcubogdan99@gmail.com>
2021-12-09 17:54:38 +0200

    added new origami model

---
+++ Models/Origami.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace origami_backend.Models
+{
+    public enum Difficulty { Easy, Medium, Hard }
+    public class Origami : BaseEntity
+    {
+        public string Name { get; set; }
+        public string PicturePath { get; set; }
+        public string Description { get; set; }
+        public string VideoPath { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public int EstimatedTime { get; set; }
+    }
+}
